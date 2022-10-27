ce14c7674ec6a48aad52daad27686f73973fe95f
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-10-27 20:05:24 +0600

    sticky nav bar added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,60 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Index from '../Components/Index'
-
-export default function Home() {
+import Head from "next/head";
+import Image from "next/image";
+import Index from "../Components/Index";
+import sanityClient, { urlFor } from "../lib/client";
+import imageUrlBuilder from "@sanity/image-url";
+const Home = ({ products, bannerData }) => {
   return (
     <>
-    <Index />
+      {/* main content */}
 
+      <Index />
+      {console.log(products)}
+
+      {/* development constent */}
+      <div className="products">
+        its a product page
+        <div>
+          {products.map((product) => (
+            <>
+              <div key={product._id}>
+                <h1>{product.name}</h1>
+                <div className="relative h-[50px] w-[50px]">
+                  <Image
+                    src={urlFor(product.image)}
+                    width={250}
+                    height={250}
+                    alt="hi"
+                    layout="fill"
+                  />
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await sanityClient.fetch(`*[_type == "product"]{
+    title,
+    name,
+    image{
+      asset->{
+        _id,
+        url
+      }
+    }
+
+  }`);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await sanityClient.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+export default Home;
