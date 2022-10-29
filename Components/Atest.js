a506ca2c8a0118e866ccbc9f833d90e46cb8d71b
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-10-29 23:57:52 +0600

    a

--- Components/Atest.js
+++ Components/Atest.js
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import sanityClient from "../lib/client";
 import imageUrlBuilder from "@sanity/image-url";
-
+//
 const Atest = ({ product: { image, name } }) => {
   const builder = imageUrlBuilder(sanityClient);
   function urlFor(sanityClient) {
@@ -13,7 +13,6 @@ const Atest = ({ product: { image, name } }) => {
       {/* {console.log(urlFor(image))} */}
       <h1>{name}</h1>
       <div className="relative h-[250px] w-[250px]">
-        <h1>{console.log(builder.image(image).width(200).url())}</h1>
         <Image src={`${builder.image(image).width(200).url()}`} alt="sorry" layout="fill" />
       </div>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,22 @@
-import Head from "next/head";
-import Image from "next/image";
+// import Head from "next/head";
+// import Image from "next/image";
 import Index from "../Components/Index";
 import sanityClient, { urlFor } from "../lib/client";
-import imageUrlBuilder from "@sanity/image-url";
 import Atest from "../Components/Atest";
 const Home = ({ products, bannerData }) => {
   return (
     <>
       {/* main content */}
-
       <Index />
       {/* {console.log(products)} */}
-
       {/* development constent */}
+      {/* mafdfEmail Design */}
       <div className="products">
         its a product page
         <div>
           {products.map((product) => (
             <>
               <Atest key={product._id} product={product} />
-                
-              
             </>
           ))}
         </div>
